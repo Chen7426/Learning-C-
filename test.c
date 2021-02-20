0d6758addcd5953336e46bba55fa78a65484e97c
Chen7426 <79237650+Chen7426@users.noreply.github.com>
2021-02-20 22:42:10 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,100 +1,75 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-
-//右移操作符>>移动的是二进制
 //int main()
 //{
-//	int a = 7;
-//	//int b = a >> 1;
-//	//int b = a >> 2;
-//	//int b = a >> 3;
-//	int b = a >> 4;
-//	printf("b = %d\n", b);
+//	short a = 0;
+//	int b = 10;
+//	printf("%d\n", sizeof(a = b + 5));
+//	printf("%d\n", a);
 //	return 0;
 //}
-
 //int main()
 //{
-//	int a = -1;
-//	int b = a << 1;
-//	//
+//	int a = 0;
+//	//原码 00000000000000000000000000000000
+//	//反码 01111111111111111111111111111111
+//	//补码 10000000000000000000000000000000
+//	int b = ~a;
 //	printf("%d\n", b);
-//	return 0;
-//}
-
-//int main()        
-//{
-//	int a = 3;
-//	int b = 5;
-//	a = a + b;//8
-//	b = a - b;//8-5为原来的a
-//	a = a - b;//8-3,为原来的b
-//	printf("a = %d , b= %d\n", a, b);
-//	return 0;
-//
-//}
-//int main()
-//{
-//	int a = 3;
-//	int b = 5;
-//	a = a^b;
-//	b = a^b;
-//	a = a^b;
-//	printf("a = %d b = %d\n",a, b);
-//	return 0;
-//}
-
-//int main()
-//{
-//	int num = 0;
-//	int count = 0;
-//	scanf("%d", &num);
-//	while (num)
-//	{
-//		if (num % 2 == 1)
-//			count++;
-//		num=num / 2;
-//	}
-//	printf("%d\n", count);
-//	return 0;
-//}
-
-//int main()
-//{
-//	int num = 0;
-//	int count = 0;
-//	scanf("%d", &num);
-//	int i = 0;
-//	for (i = 0; i < 32; i++)
-//	{
-//		if ((num & 1) == 1)
-//			count++;
-//		num = num >> i;
-//	}
-//	printf("%d\n", count);
-//	return 0;
-//}
-//int main()
-//{
-//	printf("hello world !");
 //	return 0;
 //}
 
 //int main()
 //{
 //	int a = 10;
-//	a = a + 1;
-//	printf("%d\n", a);
-//	a += 1;
-//	printf("%d\n", a);
-//
+//	//printf("%d\n", ++a);//11
+//	printf("%d\n", a++);//10
 //	return 0;
 //}
-
+//void test1(int arr[])
+//{
+//	printf("%d\n", sizeof(arr));
+//}
+//void test2(char ch[])
+//{
+//	printf("%d\n", sizeof(ch));
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	char ch[10] = { 0 };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(ch));
+//	test1(arr);
+//	test2(ch);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = a++;
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = 0;
+//	c = a&&b;
+//	printf("%d\n", c);
+//	return 0;
+//}
 int main()
 {
-	int a = 10;
-	a =+1;
-	printf("%d\n", a);
+	int a = 1;
+	int b = 10;
+	int max = 0;
+	//if ((5 > a) ? b = 3 : b = -3)
+	//b = (a > 5) ? (b = 3) : (b = -3);
+	//b = (a > 5 ? 3 : 3);
+	max = a > b ? a : b;
+	printf("%d\n", b);
+		
 	return 0;
 }
