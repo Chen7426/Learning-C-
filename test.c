959cae9cbd39841655017c0a8a9eade4ebc3a090
Chen7426 <79237650+Chen7426@users.noreply.github.com>
2021-02-22 21:56:28 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,75 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-//int main()
+//int get_max(int a, int b)
 //{
-//	short a = 0;
-//	int b = 10;
-//	printf("%d\n", sizeof(a = b + 5));
-//	printf("%d\n", a);
-//	return 0;
+//	if (a > b)
+//		return a;
+//	else
+//		return b;
 //}
 //int main()
 //{
 //	int a = 0;
-//	//原码 00000000000000000000000000000000
-//	//反码 01111111111111111111111111111111
-//	//补码 10000000000000000000000000000000
-//	int b = ~a;
-//	printf("%d\n", b);
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int max = get_max(a, b);
+//	printf("max = %d\n", max);
 //	return 0;
 //}
 
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//	char id[20];
+//};
+//int main()
+//{
+//	struct stu s1 = { "张三",20, "201742502925" };
+//
+//	struct stu* p = &s1;
+////	printf("%s\n", (*p).name);
+////	//printf("%s\n%d\n%s\n", p->name,p->age ,p->id );
+////	/*printf("%s\n", s1.name);
+////	printf("%d\n", s1.age );
+////	printf("%s\n", s1.id );*/
+////	return 0;
+////}
+////int main()
+////{
+////	char a = 3;
+////	char b = 127;
+////	char c = a + b;
+////	printf("%d\n",c);
+////}
 //int main()
 //{
 //	int a = 10;
-//	//printf("%d\n", ++a);//11
-//	printf("%d\n", a++);//10
-//	return 0;
-//}
-//void test1(int arr[])
-//{
-//	printf("%d\n", sizeof(arr));
-//}
-//void test2(char ch[])
-//{
-//	printf("%d\n", sizeof(ch));
-//}
-//int main()
-//{
-//	int arr[10] = { 0 };
-//	char ch[10] = { 0 };
-//	printf("%d\n", sizeof(arr));
-//	printf("%d\n", sizeof(ch));
-//	test1(arr);
-//	test2(ch);
-//	return 0;
-//
-//}
-//int main()
-//{
-//	int a = 0;
-//	int b = a++;
-//	printf("%d\n", a);
-//	return 0;
-//}
-//int main()
-//{
-//	int a = 1;
-//	int b = 2;
-//	int c = 0;
-//	c = a&&b;
+//	int b = 20;
+//	int c = a + b * -2;
 //	printf("%d\n", c);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 2;
+//	int b = 3;
+//	int c = 2 * 3 + 2 * 2 + 2 * 4;
+//	printf("%d\n", c);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = a + (a--);//18
+//	printf("%d\n", c);
+//	return 0;
+//}
+//int fun(int a)
+//{
+//	static int count = 1;
+//	/*a = a + count;
+//	count++;*/
+//	return ++count;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = fun(a);
+//	printf("%d\n", b);
 //	return 0;
 //}
 int main()
 {
-	int a = 1;
-	int b = 10;
-	int max = 0;
-	//if ((5 > a) ? b = 3 : b = -3)
-	//b = (a > 5) ? (b = 3) : (b = -3);
-	//b = (a > 5 ? 3 : 3);
-	max = a > b ? a : b;
+	int i = 1;
+	int b = 3 + (++i);
 	printf("%d\n", b);
-		
 	return 0;
 }
