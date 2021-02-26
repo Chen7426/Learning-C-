e18f6d64bde0f910d5c0412a6b641fcd49a00d07
Chen7426 <79237650+Chen7426@users.noreply.github.com>
2021-02-26 21:58:16 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,109 +1,167 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
-//void Init(int arr[], int sz)
+//void Print(int a)
 //{
 //	int i = 0;
-//	for (i = 0; i < sz; i++)
+//	printf("奇数位：\n");
+//	for (i = 30; i >= 0; i -= 2)
 //	{
-//		arr[i] = 0;
+//		printf("%d ", (a >> i) & 1);
 //	}
-//}
-
-//void Print(int arr[], int sz)
-//{
-//	int i = 0;
-//	for (i = 0; i < sz; i++)
+//	printf("\n");
+//	printf("偶数位：\n");
+//	for (i = 31; i >= 1; i -= 2)
 //	{
-//		printf("%d ", arr[i]);
+//		printf("%d ", (a >> i) & 1);
 //	}
 //	printf("\n");
 //}
 //
-//void Reverse(int arr[], int sz)
-//{
-//	int left = 0;
-//	int right = sz - 1;
-//	int temp = 0;
-//	while (left < right)
-//	{
-//		//int temp = arr[left];
-//		arr[left] = arr[right];
-//		temp = arr[left];
-//		arr[right] = temp;
-//		left++;
-//		right--;
-//	}
-//}
+//
 //int main()
 //{
-//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	//Init(arr, sz);
-//	Print(arr, sz);
-//	Reverse(arr, sz);
-//	Print(arr, sz);
+//	int a = 0;
+//	scanf("%d", &a);
+//	Print(a);
 //	return 0;
 //}
 //int main()
 //{
 //	int a = 10;
 //	int b = 20;
-//	int temp = 0;
-//	temp = a;
-//	a = b;
-//	b = temp;
-//	printf("a = %d b= %d\n", a, b);
+//	printf("a = %d b = %d\n", a, b);
+//	a = a + b;//30
+//	b = a - b;
+//	a = a - b;
+//	printf("a = %d b = %d\n", a, b);
 //	return 0;
+//}
+//void Print_table(int n)
+//{
+//	int i = 0;//控制行
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;//控制列
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-2d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//}
 //
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	Print_table(n);
+//	return 0;
 //}
 
 //int main()
 //{
-//	int i = 1;
-//	int ret = (++i) + (++i);
-//	/*int a = i += 2;
-//	printf("%d\n", a);*/
-//	printf("%d\n", sizeof(long));
+//	char a = '!';
+//	char b = '@';
+//	char temp = a;
+//	a = b;
+//	b = a;
+//	printf("%c %c\n", a, b);
 //	return 0;
 //}
-//int search(unsigned int a)
+
+//int main()
 //{
-//	int count = 0;
-//	
+//	int a = 10;
+//	int b = 20;
+//	printf("%d %d\n", a, b);
+//	int temp = a;
+//	a = b;
+//	b = a;
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+//void Print(int a)
+//{
+//	int b = 0;
+//	int c = 0;
 //	while (a)
 //	{
-//		if (1 == a % 2)
-//		{
-//			count++;
-//			
-//		}
-//		a = a / 2;
+//		//a = a / 10;
+//		b = a % 10;
+//		printf("%d ", b);
+//		a = a / 10;
+//		
 //	}
-//	return count;
 //}
-
-int search(int a)
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	Print(a);
+//	return 0;
+//}
+//int main()
+//{
+//	char a = '!';
+//	char b = '*';
+//	char temp = a;
+//	/*a = b;
+//	b = temp;*/
+//	char* pa = &a;
+//	char* pb = &b;
+//	*pa = b;
+//	*pb = a;
+//	printf("%c %c\n", *pa,*pb);
+//	return 0;
+//}
+//int dive_add(int a)
+//{
+//	int b = 0;
+//	int c = 0;
+//	while (a)
+//	{
+//		b = a % 10;
+//		a = a / 10;
+//		c = c + b;
+//	}
+//	return c;
+//}
+//
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int sum=dive_add(a);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+int Power(int n, int k)
 {
-	int count = 0;
+	int a = 1;
 	int i = 0;
-	for (i = 0; i < 32; i++)
+	if (n > 0)
 	{
-		if (a & 1 == 1)
+		for (i = 1; i <= k; i++)
 		{
-			count++;
+			a = a*n;
 		}
-		a = a>>1;
+		return a;
 	}
-	return count;
+	else
+		return 0;
 }
 
 
 int main()
 {
-	int a = 0;
-	scanf("%d", &a);
-	int count = search(a);
-	printf("%d\n", count);
+	int n = 0;
+	int k = 0;
+	scanf("%d%d", &n, &k);
+	int ret = Power(n, k);
+	printf("ret = %d\n", ret);
 	return 0;
 }
