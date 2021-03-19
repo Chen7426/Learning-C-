2ddadda96796b30f731f5e530bb035f6af8240a2
Chen7426 <79237650+Chen7426@users.noreply.github.com>
2021-03-19 22:25:42 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,54 +1,166 @@
-#include "contact.h"
-void menu()
-{
-	printf("***********************************************\n");
-	printf("******** 1. add            2. del   ***********\n");
-	printf("******** 3. search         4. modify***********\n");
-	printf("******** 5. show           6. sort  ***********\n");
-	printf("******** 0. exit                    ***********\n");
-	printf("***********************************************\n");
+#include <stdio.h>
+#include <stdlib.h>
+//int main()
+//{
+//	int* p = malloc(900000000000);
+//	printf("%s\n", strerror(errno));
+//	return 0;
+//}
 
-}
+//int main()
+//{
+//	int* p = (int*)calloc(10, sizeof(int));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[5] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", a[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int* p = (int*)malloc(5 * sizeof(int));
+//	if (p == NULL)
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		int i;
+//		for (i = 0; i < 5; i++)
+//		{
+//			printf("%d ",*p ++ = i);
+//		}
+//		printf("\n");
+//	}
+//	free(p);
+//	p == NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	while (1)
+//	{
+//		malloc(2);
+//		//printf("1\n");
+//	}
+//	return 0;
+//}
+
+//*************************************************************
+//实现利用函数开辟空间
+//void get(char* p)
+//{
+//	p = (char*)malloc(100);
+//}
+//
+//void test(void)
+//{
+//	char* str = NULL;
+//	get(str);
+//	strcpy(str, "hello");
+//	printf(str);
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+//
+//*************************************************************
+////改正结果1
+//void get(char** p)
+//{
+//	*p = (char*)malloc(100);
+//}
+//
+//void test(void)
+//{
+//	char* str = NULL;
+//	get(&str);
+//	strcpy(str, "hello");
+//	printf(str);
+//	free(str);
+//	str = NULL;
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+//*************************************************************
+//改正2
+//#include <string.h>
+//char* get(char* p)
+//{
+//	p = (char*)malloc(100);
+//	return p;
+//}
+//
+//void test(void)
+//{
+//	char* str = NULL;
+//	str =  get(str);
+//	strcpy(str, "hello");
+//	printf(str);
+//	free(str);
+//	str = NULL;
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+//*************************************************************
+
+//char* get()
+//{
+//	char p[] = "hello world";
+//	return p;
+//}
+//
+//void test(void)
+//{
+//	char* str = NULL;
+//	str = get();
+//	printf(str);
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+//*************************************************************
+
+//int main()
+//{
+//	int* p = 0;
+//	*p = 20;
+//	printf("%d\n", *p);
+//	return 0;
+//}
+
+struct S
+{
+	char a;
+	int* arr;//柔性数组
+};
 
 int main()
 {
-	int input = 0;
-	//创建一个可以存放1000个好友的通讯录
-	struct Contact con;
-	//初始化通讯录
-	InitContact(&con);
-
-	do
-	{
-		menu();
-		printf("请选择：>");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case ADD:
-			AddContact(&con);
-			break;
-		case DEL:
-			DelContact(&con);
-			break;
-		case SEARCH:
-			SearchContact(&con);
-			break;
-		case MODIFY:
-			ModifyContact(&con);
-			break;
-		case SHOW:
-			ShowContact(&con);
-			break;
-		case SORT:
-			break;
-		case EXIT:
-			printf("程序退出\n");
-			break;
-		default:
-			printf("输入错误，请重新输入:>\n");
-			break;
-		}
-	} while (input);
+	struct S s;
+	printf("%d\n", sizeof(s));
 	return 0;
 }
